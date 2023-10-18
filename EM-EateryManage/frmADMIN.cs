25433e22676cae8cc9825a303f43f835cf9b35db
Namhs2k3 <pnnam1103@gmail.com>
2023-10-18 13:36:45 +0700

    Chinh sua them rang buoc cho chuc nang them o form ADMIN

--- EM-EateryManage/frmADMIN.cs
+++ EM-EateryManage/frmADMIN.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace EM_EateryManage
 {
@@ -19,6 +20,12 @@ namespace EM_EateryManage
         {
 
             InitializeComponent();
+        }
+        private void frmADMIN_Load(object sender, EventArgs e)
+        {
+            AddDataToDGV();
+            AddDataToDGV_BanAn();
+            AddDataToDGV_TaiKhoan();
         }
         public void AddDataToDGV()
         {
@@ -119,49 +126,178 @@ namespace EM_EateryManage
                     command.CommandText = query;
                     command.Parameters.Clear();
 
-                    if (txtAdd_TenMon.Text != "" && txtAdd_Gia.Text != "")
+                    bool usernameExists = false;
+                    int total = dgvDSMonAn.Rows.Count - 1;
+                    for (int i = 0; i < total; i++)
                     {
-                        command.Parameters.AddWithValue("@name", txtAdd_TenMon.Text);
-                        command.Parameters.AddWithValue("@price", gia);
+                        if (txtAdd_TenMon.Text == dgvDSMonAn.Rows[i].Cells[1].Value.ToString())
+                        {
+                            usernameExists = true;
+                            break;
+                        }
+                    }
+
+                    if(usernameExists)
+                    {
+                        MessageBox.Show("Món Ăn Đã Tồn Tại!","Thông Báo!", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                        txtAdd_TenMon.Focus();
+                        return;
                     }
                     else
                     {
-                        MessageBox.Show("Vui Lòng Nhập Đầy Đủ Tên và Giá!", "Thông Báo");
+                        if (txtAdd_TenMon.Text != "" && txtAdd_Gia.Text != "")
+                        {
+                            command.Parameters.AddWithValue("@name", txtAdd_TenMon.Text);
+                            command.Parameters.AddWithValue("@price", gia);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Vui Lòng Nhập Đầy Đủ Tên và Giá!", "Thông Báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                        }
+                        command.Parameters.AddWithValue("@Image", txtAdd_LinkIMG.Text);
+                        command.Parameters.AddWithValue("@material", txtAdd_Loai.Text);
+                        command.Parameters.AddWithValue("@detail", txtAdd_MoTa.Text);
+
+                        command.ExecuteNonQuery();
+
+                        MessageBox.Show("Thêm hàng thành công!", "Thông Báo!", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                        AddDataToDGV();
+                        connection.Close();
                     }
-                    command.Parameters.AddWithValue("@Image", txtAdd_LinkIMG.Text);
-                    command.Parameters.AddWithValue("@material", txtAdd_Loai.Text);
-                    command.Parameters.AddWithValue("@detail", txtAdd_MoTa.Text);
-
-                    command.ExecuteNonQuery();
-                    
-                    MessageBox.Show("Thêm hàng thành công!");
-                    AddDataToDGV();
-                    connection.Close();
                 }
             }
-            catch (Exception ex)
+            catch //(Exception ex)
             {
-                if (ex is FormatException)
+                //MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+        private void btnThemBan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
                 {
-                    MessageBox.Show("Giá trị giá tiền không hợp lệ!");
+                    connection.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.Connection = connection;
+
+
+                    string query = "INSERT INTO QuanLyBan (ten_Ban, so_ghe) VALUES (@name, @SLGhe)";
+                    command.CommandText = query;
+                    command.Parameters.Clear();
+
+                    bool usernameExists = false;
+                    int total = dgvBanAn.Rows.Count - 1;
+                    for (int i = 0; i < total; i++)
+                    {
+                        if (txtTenBan.Text == dgvBanAn.Rows[i].Cells[1].Value.ToString())
+                        {
+                            usernameExists = true;
+                            break;
+                        }
+                    }
+
+                    if(usernameExists)
+                    {
+                        MessageBox.Show("Bàn Đã Tồn Tại!", "Thông Báo!", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                        txtAdd_TenMon.Focus();
+                        return;
+                    }
+                    else
+                    {
+                        if (txtTenBan.Text != "" && cbbSLGhe.Text != "")
+                        {
+                            command.Parameters.AddWithValue("@name", txtTenBan.Text);
+                            command.Parameters.AddWithValue("@SLGhe", cbbSLGhe.Text);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Vui Lòng Nhập Đầy Đủ!", "Thông Báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                        }
+
+
+                        command.ExecuteNonQuery();
+
+                        MessageBox.Show("Thêm bàn thành công!", "Thông Báo!", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                        AddDataToDGV_BanAn();
+                        connection.Close();
+                    }
                 }
-                else
-                {
-                    MessageBox.Show("An error occurred: " + ex.Message);
-                }
+            }
+            catch //(Exception ex)
+            {
+                //MessageBox.Show("An error occurred: " + ex.Message);
             }
         }
 
-        private void frmADMIN_Load(object sender, EventArgs e)
+        private void btnThemTK_Click(object sender, EventArgs e)
         {
-            AddDataToDGV();
-            AddDataToDGV_BanAn();
-            AddDataToDGV_TaiKhoan();
-        }
+            using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.Connection = connection;
 
-        private void btnSua_Click(object sender, EventArgs e)
-        {
+                    string query = "INSERT INTO Account (display_name, username, password, Chuc_Vu) VALUES (@1, @2, @3, @4)";
+                    command.CommandText = query;
+                    command.Parameters.Clear();
 
+                    // Kiểm tra xem tên tài khoản được thêm vào đã tồn tại trong csdl hay chưa
+                    bool usernameExists = false;
+                    int total = dgvACC.Rows.Count - 1;
+                    for (int i = 0; i < total; i++)
+                    {
+                        if (txtTenDN_TK.Text == dgvACC.Rows[i].Cells[2].Value.ToString())
+                        {
+                            usernameExists = true;
+                            break;
+                        }
+                    }
+
+                    if (usernameExists)
+                    {
+                        MessageBox.Show("Tài Khoản Đã Tồn Tại!", "Thông Báo!", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                        txtTenDN_TK.Focus();
+                        return;
+                    }
+                    else
+                    {
+                        if ((txtTenDN_TK.Text == "" && txtTenHT_TK.Text == "" && txtMK_TK.Text == ""))
+                        {
+                            MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông Báo!", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                            txtTenDN_TK.Focus();
+                        }
+                        else
+                        {
+                            // Nếu tên tài khoản không tồn tại, thì thêm tài khoản mới
+                            command.Parameters.AddWithValue("@1", txtTenHT_TK.Text);
+                            command.Parameters.AddWithValue("@2", txtTenDN_TK.Text);
+                            command.Parameters.AddWithValue("@3", txtMK_TK.Text);
+
+                            if (cbLoaiTK_TK.Text == "admin" || cbLoaiTK_TK.Text == "staff")
+                            {
+                                command.Parameters.AddWithValue("@4", cbLoaiTK_TK.Text);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Loại Tài Khoản Không Hợp Lệ!\nVui Lòng Chọn Loại admin Hoặc staff!", "Thông Báo");
+                                return;
+                            }
+
+                            command.ExecuteNonQuery();
+
+                            MessageBox.Show("Thêm Tài Khoản Thành Công!", "Thông Báo!", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                            AddDataToDGV_TaiKhoan();
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+            }
         }
 
         private void txtAdd_Gia_TextChanged(object sender, EventArgs e)
@@ -189,313 +325,19 @@ namespace EM_EateryManage
             }
         }
 
-        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dgvDanhMuc_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void tpTaiKhoan_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tpThucAn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dgvDSMonAn_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void btnXoa_Click(object sender, EventArgs e)
-        {
-
-        }
-
-
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
         private void btnTimMon_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void txtAdd_TenMon_TextChanged(object sender, EventArgs e)
+        private void btnXoaMon_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void txtAdd_LinkIMG_TextChanged(object sender, EventArgs e)
+        private void btnSuaMon_Click(object sender, EventArgs e)
         {
-
+            
         }
-
-        private void txtAdd_Loai_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtAdd_MoTa_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tpDanhMuc_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tpBanAn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tpDoanhThu_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panel4_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void btnDisplayBill_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dtpkNgayDi_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void dtpkNgayDen_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dgvBill_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void panel5_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panel6_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void txtID_DM_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtTenDM_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel7_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnThemDM_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnSuaDM_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnXoaDM_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnXemDM_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label9_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label10_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void btnThemBan_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
-                {
-                    connection.Open();
-                    SqlCommand command = new SqlCommand();
-                    command.Connection = connection;
-
-                    
-                    string query = "INSERT INTO QuanLyBan (ten_Ban, so_ghe) VALUES (@name, @SLGhe)";
-                    command.CommandText = query;
-                    command.Parameters.Clear();
-
-                    if ( txtTenBan.Text != "" && cbbSLGhe.Text != "")
-                    {
-                        command.Parameters.AddWithValue("@name", txtTenBan.Text);
-                        command.Parameters.AddWithValue("@SLGhe", cbbSLGhe.Text);
-                    }
-                    else
-                    {
-                        MessageBox.Show("Vui Lòng Nhập Đầy Đủ!", "Thông Báo");
-                    }
-                    
-
-                    command.ExecuteNonQuery();
-
-                    MessageBox.Show("Thêm bàn thành công!");
-                    AddDataToDGV_BanAn();
-                    connection.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                if (ex is FormatException)
-                {
-                    MessageBox.Show("Giá trị giá tiền không hợp lệ!");
-                }
-                else
-                {
-                    MessageBox.Show("An error occurred: " + ex.Message);
-                }
-            }
-        }
-
-        private void btnThemTK_Click(object sender, EventArgs e)
-        {
-            using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
-            {
-                try
-                {
-                    connection.Open();
-                    SqlCommand command = new SqlCommand();
-                    command.Connection = connection;
-
-                    string query = "INSERT INTO Account (display_name, username, password, Chuc_Vu) VALUES (@1, @2, @3, @4)";
-                    command.CommandText = query;
-                    command.Parameters.Clear();
-
-                    // Kiểm tra xem tên tài khoản được thêm vào đã tồn tại trong csdl hay chưa
-                    bool usernameExists = false;
-                    int total = dgvACC.Rows.Count-1;
-                    for (int i =0; i<total; i++)
-                    {
-                        if (txtTenDN_TK.Text == dgvACC.Rows[i].Cells[2].Value.ToString())
-                        {
-                            usernameExists = true;
-                            break;
-                        }
-                    }
-
-                    if (usernameExists)
-                    {
-                        MessageBox.Show("Tài Khoản Đã Tồn Tại!", "Thông Báo!");
-                        return;
-                    }
-                    else
-                    {
-                        // Nếu tên tài khoản không tồn tại, thì thêm tài khoản mới
-                        command.Parameters.AddWithValue("@1", txtTenHT_TK.Text);
-                        command.Parameters.AddWithValue("@2", txtTenDN_TK.Text);
-                        command.Parameters.AddWithValue("@3", txtMK_TK.Text);
-
-                        if (cbLoaiTK_TK.Text == "admin" || cbLoaiTK_TK.Text == "staff")
-                        {
-                            command.Parameters.AddWithValue("@4", cbLoaiTK_TK.Text);
-                        }
-                        else
-                        {
-                            MessageBox.Show("Loại Tài Khoản Không Hợp Lệ!\nVui Lòng Chọn Loại admin Hoặc staff!", "Thông Báo");
-                            return;
-                        }
-
-                        command.ExecuteNonQuery();
-
-                        MessageBox.Show("Thêm Tài Khoản Thành Công!");
-                        AddDataToDGV_TaiKhoan();
-                    }
-
-                    
-                }
-                catch (FormatException ex)
-                {
-                    MessageBox.Show("Giá trị giá tiền không hợp lệ!");
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("An error occurred: " + ex.Message);
-                }
-                finally
-                {
-                    connection.Close();
-                }
-            }
-        }
-
     }
 }
