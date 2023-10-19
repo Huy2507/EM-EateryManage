f2bcbd03013833eedc38d7994f15641e29aee525
sang <nts25112003@gmail.com>
2023-10-19 23:59:46 +0700

    Them form bill va xu ly thanh toan

---
+++ EM-EateryManage/frmBill.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace EM_EateryManage
+{
+    partial class frmBill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbTable = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.lbHoaDon = new System.Windows.Forms.Label();
+            this.btnThanhToan = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbToTal = new Guna.UI2.WinForms.Guna2TextBox();
+            this.SuspendLayout();
+            // 
+            // cbTable
+            // 
+            this.cbTable.BackColor = System.Drawing.Color.Transparent;
+            this.cbTable.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cbTable.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbTable.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTable.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbTable.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbTable.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cbTable.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cbTable.ItemHeight = 30;
+            this.cbTable.Location = new System.Drawing.Point(0, 0);
+            this.cbTable.Name = "cbTable";
+            this.cbTable.Size = new System.Drawing.Size(800, 36);
+            this.cbTable.TabIndex = 0;
+            this.cbTable.SelectedIndexChanged += new System.EventHandler(this.cbTable_SelectedIndexChanged);
+            // 
+            // lbHoaDon
+            // 
+            this.lbHoaDon.AutoSize = true;
+            this.lbHoaDon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHoaDon.Location = new System.Drawing.Point(113, 77);
+            this.lbHoaDon.Name = "lbHoaDon";
+            this.lbHoaDon.Size = new System.Drawing.Size(64, 25);
+            this.lbHoaDon.TabIndex = 1;
+            this.lbHoaDon.Text = "label1";
+            // 
+            // btnThanhToan
+            // 
+            this.btnThanhToan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThanhToan.Location = new System.Drawing.Point(531, 337);
+            this.btnThanhToan.Name = "btnThanhToan";
+            this.btnThanhToan.Size = new System.Drawing.Size(192, 49);
+            this.btnThanhToan.TabIndex = 2;
+            this.btnThanhToan.Text = "Thanh Toán";
+            this.btnThanhToan.UseVisualStyleBackColor = true;
+            this.btnThanhToan.Click += new System.EventHandler(this.btnThanhToan_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Total";
+            // 
+            // txbToTal
+            // 
+            this.txbToTal.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbToTal.DefaultText = "";
+            this.txbToTal.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txbToTal.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txbToTal.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txbToTal.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txbToTal.Enabled = false;
+            this.txbToTal.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbToTal.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txbToTal.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbToTal.Location = new System.Drawing.Point(118, 337);
+            this.txbToTal.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txbToTal.Name = "txbToTal";
+            this.txbToTal.PasswordChar = '\0';
+            this.txbToTal.PlaceholderText = "";
+            this.txbToTal.SelectedText = "";
+            this.txbToTal.Size = new System.Drawing.Size(298, 38);
+            this.txbToTal.TabIndex = 4;
+            // 
+            // frmBill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txbToTal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnThanhToan);
+            this.Controls.Add(this.lbHoaDon);
+            this.Controls.Add(this.cbTable);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmBill";
+            this.Text = "frmBill";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ComboBox cbTable;
+        private System.Windows.Forms.Label lbHoaDon;
+        private System.Windows.Forms.Button btnThanhToan;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2TextBox txbToTal;
+    }
+}
