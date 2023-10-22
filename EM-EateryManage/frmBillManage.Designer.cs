0504c9e4b7d514e96994f1645868be47e2984f81
sang <nts25112003@gmail.com>
2023-10-22 20:26:42 +0700

    Xử lý form manage bill

--- EM-EateryManage/frmBillManage.Designer.cs
+++ EM-EateryManage/frmBillManage.Designer.cs
@@ -30,20 +30,23 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnViewBill = new System.Windows.Forms.Button();
-            this.dttpToDate = new Guna.UI2.WinForms.Guna2DateTimePicker();
             this.dttpFromDate = new Guna.UI2.WinForms.Guna2DateTimePicker();
             this.panel2 = new System.Windows.Forms.Panel();
             this.dtgvManageBill = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbSearchId = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dtgvManageBill)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.AntiqueWhite;
             this.panel1.Controls.Add(this.btnViewBill);
-            this.panel1.Controls.Add(this.dttpToDate);
             this.panel1.Controls.Add(this.dttpFromDate);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -54,39 +57,27 @@
             // btnViewBill
             // 
             this.btnViewBill.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.btnViewBill.Location = new System.Drawing.Point(406, 25);
+            this.btnViewBill.Location = new System.Drawing.Point(513, 26);
             this.btnViewBill.Name = "btnViewBill";
             this.btnViewBill.Size = new System.Drawing.Size(226, 58);
             this.btnViewBill.TabIndex = 2;
             this.btnViewBill.Text = "Thống kê";
             this.btnViewBill.UseVisualStyleBackColor = true;
-            // 
-            // dttpToDate
-            // 
-            this.dttpToDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.dttpToDate.Checked = true;
-            this.dttpToDate.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.dttpToDate.Format = System.Windows.Forms.DateTimePickerFormat.Long;
-            this.dttpToDate.Location = new System.Drawing.Point(638, 25);
-            this.dttpToDate.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
-            this.dttpToDate.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
-            this.dttpToDate.Name = "dttpToDate";
-            this.dttpToDate.Size = new System.Drawing.Size(386, 59);
-            this.dttpToDate.TabIndex = 1;
-            this.dttpToDate.Value = new System.DateTime(2023, 10, 21, 23, 36, 18, 329);
+            this.btnViewBill.Click += new System.EventHandler(this.btnViewBill_Click);
             // 
             // dttpFromDate
             // 
             this.dttpFromDate.Checked = true;
+            this.dttpFromDate.CustomFormat = "MM-dd-yyyy";
             this.dttpFromDate.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.dttpFromDate.Format = System.Windows.Forms.DateTimePickerFormat.Long;
+            this.dttpFromDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dttpFromDate.Location = new System.Drawing.Point(14, 26);
             this.dttpFromDate.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
             this.dttpFromDate.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
             this.dttpFromDate.Name = "dttpFromDate";
             this.dttpFromDate.Size = new System.Drawing.Size(386, 58);
             this.dttpFromDate.TabIndex = 0;
-            this.dttpFromDate.Value = new System.DateTime(2023, 10, 21, 23, 35, 8, 609);
+            this.dttpFromDate.Value = new System.DateTime(2023, 11, 25, 23, 59, 0, 0);
             // 
             // panel2
             // 
@@ -108,11 +99,64 @@
             this.dtgvManageBill.Size = new System.Drawing.Size(1062, 537);
             this.dtgvManageBill.TabIndex = 0;
             // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.txbSearchId);
+            this.panel3.Controls.Add(this.btnSearch);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 539);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1062, 102);
+            this.panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tìm kiếm theo ID";
+            // 
+            // txbSearchId
+            // 
+            this.txbSearchId.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbSearchId.DefaultText = "";
+            this.txbSearchId.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txbSearchId.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txbSearchId.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txbSearchId.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txbSearchId.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbSearchId.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txbSearchId.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbSearchId.Location = new System.Drawing.Point(381, 20);
+            this.txbSearchId.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txbSearchId.Name = "txbSearchId";
+            this.txbSearchId.PasswordChar = '\0';
+            this.txbSearchId.PlaceholderText = "";
+            this.txbSearchId.SelectedText = "";
+            this.txbSearchId.Size = new System.Drawing.Size(253, 59);
+            this.txbSearchId.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(804, 20);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(220, 61);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
             // frmBillManage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1062, 641);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -121,6 +165,8 @@
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dtgvManageBill)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -130,8 +176,11 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnViewBill;
-        private Guna.UI2.WinForms.Guna2DateTimePicker dttpToDate;
         private Guna.UI2.WinForms.Guna2DateTimePicker dttpFromDate;
         private System.Windows.Forms.DataGridView dtgvManageBill;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2TextBox txbSearchId;
+        private System.Windows.Forms.Button btnSearch;
     }
 }
